ce89f3c6dd8310081f75f5542010cbcb32bbfaae
Scott-Crawley <scott.crawley@ada.ac.uk>
2021-04-30 14:11:06 +0100

    Pass CI tests

--- tests/convertToGBP.test.js
+++ tests/convertToGBP.test.js
@@ -6,13 +6,13 @@ describe("TEST: convertToGBP()", () => {
 
 describe('Testing the convertToGBP() function', () => {
     it('Tests for valid inputs', () => {
-        expect(convertToGBP(100, 'EUR')).toBe('86.96')
-        expect(convertToGBP(100, 'USD')).toBe('71.43')
-        expect(convertToGBP(100, 'RUB')).toBe('0.96')
+        expect(convertToGBP(100, 'EUR')).toBe('86.96');
+        expect(convertToGBP(100, 'USD')).toBe('71.43');
+        expect(convertToGBP(100, 'RUB')).toBe('0.96');
     })
     it('Tests for invalid inputs', () => {
-        expect(convertToGBP('500', 'EUR')).toBeFalsy()      
-        expect(convertToGBP(500, 'GHJ')).toBeFalsy() 
-        expect(convertToGBP(undefined, 'BNM')).toBeFalsy()
+        expect(convertToGBP('500', 'EUR')).toEqual(0);      
+        expect(convertToGBP(500, 'GHJ')).toEqual(0);
+        expect(convertToGBP(undefined, 'BNM')).toEqual(0);
     })
 })
