818210d738b1dd4794e90763d5fa33a77b8ea2e5
Ethan Moss <ethan.moss@ada.ac.uk>
2021-04-30 12:41:10 +0100

    tests for valid and invalid inputs

--- tests/convertGBP.test.js
+++ tests/convertGBP.test.js
@@ -16,5 +16,6 @@ describe('Testing the convertGBP function', () => {
         expect(convertToGBP('500', 'EUR')).toBeFalsy()      
         expect(convertToGBP(500, 'GHJ')).toBeFalsy() 
         expect(convertToGBP(undefined, 'BNM')).toBeFalsy()
+        expect(convertToGBP()).toBeFalsy()
     })
 })
