1d3a360e8143d34656c7be08ba3b1c2ae2d6ca31
Scott-Crawley <74011247+Scott-Crawley@users.noreply.github.com>
2021-04-30 12:09:22 +0100

    Revert "Misc improvements"

--- tests/convertGBP.test.js
+++ tests/convertGBP.test.js
@@ -1,15 +1,6 @@
-const { convertToGBP } = require('../converters/convertGBP')
 
 describe("TEST: convertGBP()", () => {
     test("Smoke Test", () => {
         expect(1).toBe(1);
     });
 });
-
-describe('Testing the convertGBP function', () => {
-    it('Tests for valid inputs', () => {
-        expect(convertToGBP(100, 'EUR')).toBe('86.96')
-        expect(convertToGBP(100, 'USD')).toBe('71.43')
-        expect(convertToGBP(100, 'RUB')).toBe('0.96')
-    })
-})
